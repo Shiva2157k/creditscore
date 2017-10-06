fe04424a5033a87527a851bb7842341097a5d724
shiva2157k <shivak916@gmail.com>
2017-10-06 01:08:54 -0500

    (app-featPopup)

--- src/popup.js
+++ src/popup.js
@@ -4,19 +4,41 @@ import React, { Component } from 'react';
 
      constructor() {
          super();
+         this.hidePopup = this.hidePopup.bind(this);
+     }
+
+     hidePopup(){
+         this.props.hidePopup();
+
      }
 
      render() {
-         return (
-             <div>
-                 <div>
-                     <label>
-                         {this.props.message}
-                     </label>
+         var html = this.props.showPopup ? (
+         <div className="modal fade">
+             <div className="modal-dialog" role = "document">
+                 <div className="modal-content">
+                     <div className="modal-header">
+                         <h5 className="modal-title">Status</h5>
+                         <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.hidePopup}>
+                             <span aria-hidden="true">&times;</span>
+                         </button>
+                     </div>
+                     <div className="modal-body">
+                         <p>{this.props.message}</p>
+                     </div>
                  </div>
+
              </div>
-         )
+
+         </div>): null;
+
+         return (
+             html
+
+         );
+
      }
+
  }
 
 
